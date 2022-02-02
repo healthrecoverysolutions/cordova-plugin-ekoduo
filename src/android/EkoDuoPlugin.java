a8716d7884b8118849a3a7e560056c432ad77202
Nehal Dutta <nehaldutta7@gmail.com>
2022-02-02 17:58:08 +0530

    added scan functionality and removed ionic wrapper folder

--- src/android/EkoDuoPlugin.java
+++ src/android/EkoDuoPlugin.java
@@ -1,26 +1,87 @@
 package cordova.plugin.ekoduo;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.ekodevices.ekoconnect.EkoConnect;
+import com.ekodevices.ekoconnect.data.entities.device.ECDevice;
+import com.ekodevices.ekoconnect.data.entities.device.ECScanResult;
+import com.ekodevices.ekoconnect.eventlisteners.external.ECDeviceConnectListener;
+import com.ekodevices.ekoconnect.eventlisteners.external.ECDeviceScanListener;
+import com.ekodevices.ekoconnect.eventlisteners.external.ECStreamListener;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import kotlin.Unit;
+
+
 /**
  * This class echoes a string called from JavaScript.
  */
-public class EkoDuoPlugin extends CordovaPlugin {
+public class EkoDuoPlugin extends CordovaPlugin implements  ECDeviceScanListener, ECDeviceConnectListener, ECStreamListener {
+
+    public static EkoConnect instance;
+    public static ECDeviceScanListener ecDeviceScanListener;
+    public static ECDeviceConnectListener ecDeviceConnectListener;
+    private CallbackContext deviceDiscoveredCallback;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Log.d("AB", "Execute with action " + action);
         if (action.equals("coolMethod")) {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
             return true;
+        } else if(action.equals("nativeToast")){
+            Log.d("AB", " Native Call to show TOAST -- ");
+            nativeToast();
+        } else if(action.equals("initializeSdk")) {
+            Log.d("AB", "Initialize SDK -- ");
+//            Handler handler = new Handler(Looper.getMainLooper());
+//            handler.post(new Runnable() {
+//                public void run() {
+//                    initializeSdk();
+//                }
+//            });
+
+            cordova.getActivity().runOnUiThread(
+                new Runnable() {
+                    public void run() {
+                        initializeSdk();
+                    }
+                });
+
+           // initializeSdk();
+//            cordova.getThreadPool().execute(new Runnable() {
+//                public void run() {
+// initializeSdk();
+//                }
+//        });
+        } else if(action.equals("startDeviceDiscovery")) {
+           // instance.setDeviceScanListener(this::foundDevice);
+         //   instance.getScannedDevicesLiveData().observeForever(this::foundDevice);
+            deviceDiscoveredCallback = callbackContext;
+            startDeviceDiscovery();
+            PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+            result.setKeepCallback(true);
+            callbackContext.sendPluginResult(result);
+        } else if(action.equals("connect")) {
+            instance.connect(scannedEkoDevice);
         }
         return false;
     }
+    ECScanResult scannedEkoDevice;
 
     private void coolMethod(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
@@ -29,4 +90,117 @@ public class EkoDuoPlugin extends CordovaPlugin {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
+
+    public void nativeToast(){
+        Toast.makeText(webView.getContext(), "Hello World Cordova Plugin", Toast.LENGTH_LONG).show();
+    }
+
+    private static String ekoBaseUrl = "https://dashboard.ekodevices.com/";
+    private static String apiKey = "af951a54474122251b23ff27db4d71e80e5092b7a0e854cd085bb1b2273c617f";
+    private static String secretKey = "01d6508f6abe9991a2c77f68ff87868659ec4e06a4c16f7c81c1459f298f3367";
+
+    public void initializeSdk() {
+        Log.d("AB", "Initializing the EKo SDK ###");
+        instance = EkoConnect.Companion.getInstance(webView.getContext());
+        Log.d("AB", "Initialized the instace -- " + instance);
+        instance.initEkoConnect(ekoBaseUrl, apiKey, secretKey, "pcm_id", authorizationStatus -> Unit.INSTANCE);
+        Log.d("AB", "setting the scan listener ");
+        instance.setDeviceScanListener(this::foundDevice);
+        instance.getScannedDevicesLiveData().observeForever(this::foundDevice);
+       instance.getConnectedDeviceLiveData().observeForever(this::connectedToDevice);
+       instance.setConnectListener(ecDeviceConnectListener);
+        Log.d("AB", "Starting Device Discovery  for Eko device");
+        // instance.startDeviceDiscovery();
+    }
+
+    public void startDeviceDiscovery(){
+        instance.startDeviceDiscovery();
+    }
+
+    private JSONObject ekoDeviceToJSON(ECScanResult device) throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("name", device.getName());
+        json.put("address", device.getAddress());
+        json.put("id", device.getAddress());
+        return json;
+    }
+
+     @Override
+    public void foundDevice(@NonNull ECScanResult ecScanResult) {
+        Log.d("AB", "Device found called ");
+        if (instance != null && ecScanResult != null && !isDeviceConnected ) {
+            Log.d("AB", "Eko Device Found " + ecScanResult);
+            Log.d("AB", "Eko Device Found name " + ecScanResult.getName());
+            Log.d("AB", "Eko Device Found address " + ecScanResult.getAddress());
+            Log.d("AB", "Eko Device Found tyoe " + ecScanResult.getType().name());
+            try {
+            JSONObject scannedPeripheral = ekoDeviceToJSON(ecScanResult);
+            Log.d("AB", "Scanned peripheral  == " + scannedPeripheral.toString());
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, scannedPeripheral);
+            pluginResult.setKeepCallback(true);
+            deviceDiscoveredCallback.sendPluginResult(pluginResult);
+
+            //temporary till UI is not present
+                scannedEkoDevice = ecScanResult;
+            } catch (JSONException e) {
+                // This shouldn't happen, log and ignore
+                Log.e("AB", "Problem converting device to JSON", e);
+            }
+            // instance.connect(ecScanResult);
+        }
+    }
+    boolean isDeviceConnected = false;
+
+    @Override
+    public void connectedToDevice(@Nullable ECDevice ecDevice) {
+        Log.d("AB", "Connected to eko device");
+        isDeviceConnected = true;
+        boolean startStreaming = false;
+        if (ecDevice != null) {
+            Log.d("AB", "Device is connected and ready to transmit " + ecDevice.getAddress());
+            Log.d("AB", "As device is successfully conected will start fetching stream data now ");
+
+            if(!startStreaming) {
+                startStreaming = true;
+                instance.startStreaming(this);
+            }
+        }
+    }
+
+    @Override
+    public void deviceDidDisconnect(@Nullable ECDevice ecDevice, @Nullable String s) {
+        Log.d("AB", "Did device eko disconnect ? ");
+    }
+
+    @Override
+    public void failedToConnectToDevice(@Nullable String s, @Nullable ECDevice ecDevice, @Nullable String s1) {
+        Log.d("AB", "Failed to connect  to eko device");
+    }
+
+    @Override
+    public void onDisplayableAudioDataPoints(@Nullable short[] shorts) {
+        Log.d("AB", "######### AUDIO DATA POINT " + shorts.length);
+        //localAudioStreamingManager.feedAudio(shorts);
+
+    }
+
+    @Override
+    public void onFilteredECGDataPoints(@Nullable short[] shorts) {
+        Log.d("AB", " ************** ECG DATA POINT " + shorts.length);
+    }
+
+    @Override
+    public void onRecordingCommandReceived() {
+        Log.d("AB", "On Recording command recieved ");
+    }
+
+
+    @Override
+    public void onPacketData(@Nullable short[] shorts, @Nullable short[] shorts1) {
+
+    }
+
+
 }
+
+
--- www/EkoDuoPlugin.js
+++ www/EkoDuoPlugin.js
@@ -1,5 +1,59 @@
-var exec = require('cordova/exec');
+/* global cordova, module */
+"use strict";
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'EkoDuoPlugin', 'coolMethod', [arg0]);
+var stringToArrayBuffer = function(str) {
+    var ret = new Uint8Array(str.length);
+    for (var i = 0; i < str.length; i++) {
+        ret[i] = str.charCodeAt(i);
+    }
+    return ret.buffer;
 };
+
+var base64ToArrayBuffer = function(b64) {
+    return stringToArrayBuffer(atob(b64));
+};
+
+function massageMessageNativeToJs(message) {
+    if (message.CDVType == 'ArrayBuffer') {
+        message = base64ToArrayBuffer(message.data);
+    }
+    return message;
+}
+
+function convertToNativeJS(object) {
+    Object.keys(object).forEach(function (key) {
+        var value = object[key];
+        object[key] = massageMessageNativeToJs(value);
+        if (typeof(value) === 'object') {
+            convertToNativeJS(value);
+        }
+    });
+}
+
+module.exports = {
+    nativeToast: function (arg0, success, error) {
+        cordova.exec(success, error, 'EkoDuoPlugin', 'nativeToast', [arg0]);
+    },
+    
+    initializeSdk: function(arg0, success, error) {
+          cordova.exec(success, error, 'EkoDuoPlugin', 'initializeSdk', [arg0]);
+    },
+    
+    startDeviceDiscovery: function (success, failure) {
+//        var successWrapper = function(peripheral) {
+//                 convertToNativeJS(peripheral);
+//                 success(peripheral);
+//             };
+        cordova.exec(/*successWrapper*/ success, failure, 'EkoDuoPlugin', 'startDeviceDiscovery', []);
+    },
+
+    connect: function (success, failure) {
+        cordova.exec( success, failure, 'EkoDuoPlugin', 'connect', []);
+    }
+    
+};
+
+module.exports.withPromises = {
+    startDeviceDiscovery: module.exports.startDeviceDiscovery
+};
+
